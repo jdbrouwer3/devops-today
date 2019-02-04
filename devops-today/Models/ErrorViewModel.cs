a4424c360f6498246ed4d66e824df3fc77976c9a
John <jdbrouwer3@gmail.com>
2019-02-04 15:59:28 -0500

    add mvc solution and project

---
+++ devops-today/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace devops_today.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
